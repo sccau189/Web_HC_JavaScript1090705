3df7e88d04e9bb15246eafdc5dc5c74ce92d63e8
sccau189 <sccau189@hotmail.com>
2020-07-12 12:25:30 +0800

    var與let

--- main.js
+++ main.js
@@ -32,14 +32,14 @@ console.log(e);
 // ES6 指數符號 **
 
 var f = 5;
-f = f**3
+f = f ** 3
 console.log(f);
 
 // 指定運算子，適用於+ - * / % **
-var num1 =10;
+var num1 = 10;
 // num1 = num1 + 5
 num1 **= 5;
-console.log("指定運算子"+ num1);
+console.log("指定運算子" + num1);
 
 // 自訂函式(方式) function、method
 // 語法 :
@@ -50,18 +50,18 @@ function functionA() {
 // 需要呼叫才會執行 functionA 是函式名字
 functionA();
 
-function addTen (number) {
+function addTen(number) {
 
-    number +=10;
+    number += 10;
 
     console.log("加十" + number)
-    
+
 }
 
 addTen(7);
 addTen(100);
 
-function mul(a, b){
+function mul(a, b) {
     console.log("數字相乘的結果" + (a * b));
 }
 
@@ -71,9 +71,9 @@ mul(155, 10)
 // 物件 {物件資料名稱 : 物件資料值}
 var car = {
     brand: "BMW",
-    cc : 2000,
+    cc: 2000,
     color: "gold",
-    drive: function() {
+    drive: function () {
         console.log("開車中...")
     }
 }
@@ -84,7 +84,7 @@ car.drive();
 
 // 練習事件
 
-var box2 =document.getElementById("box2");
+var box2 = document.getElementById("box2");
 
 box2.onclick = () => {
     console.log("滑入!");
@@ -94,7 +94,7 @@ box2.onmouseenter = () => {
     console.log("滑入!");
 }
 
-box2.onmouseleave= () => {
+box2.onmouseleave = () => {
     console.log("滑出!");
 }
 
@@ -115,11 +115,11 @@ var numbers = [10, 100, 40, 9]
 
 console.log("陣列第三筆資料:" + numbers[2]);
 
-numbers[0] =999;
+numbers[0] = 999;
 
 console.log("陣列第一筆資料:" + numbers[0]);
 
-console.log("陣列的數量:" + numbers.length); 
+console.log("陣列的數量:" + numbers.length);
 
 // 比較運算子 > < >= <= == !=
 var testA = 100;
@@ -131,7 +131,7 @@ console.log(testA != testB);
 var testC = 7;
 var testD = "7";
 
-console.log("== 的結果:" +(testC == testD));  //比較值
+console.log("== 的結果:" + (testC == testD));  //比較值
 console.log("=== 的結果:" + (testC === testD));  //比較值與類型
 
 // == 與 === 的差異(練習2)
@@ -163,3 +163,95 @@ console.log(true || false)    //true
 console.log(false || true)    //true
 console.log(false || false)   //false
 
+// 判斷式 if
+// 語法 Syntax
+// if (布林值) {當布林值為 true 會執行 }
+
+if (true) {
+    console.log("我是判斷式")
+}
+
+// else 否則
+// if (布林值) {當布林值為 true 會執行 }
+// else {當布林值為 false 會執行}
+
+var boolTest = false;
+
+if (boolTest) {
+    console.log("布林值為 true!")
+} else {
+    console.log("布林值為 fasle!")
+}
+
+// 如果 分數 >= 60 :及格
+// 如果 分數 <= 60 :補考
+// 如果 分數 <= 40 :被當
+var score = document.getElementById("score");
+var result = document.getElementById("result")
+
+score.onchange = () => {
+    var s = parseInt(score.value);
+
+    // if (s >=60) {
+    //     result.innerText="變更中....";
+    // } 語法
+    // if   else if   else 按照順序 不能互換
+    if (s >= 60) {
+        result.innerText = "分數:" + s + "-及格";
+    }
+    else if (s >= 40) {
+        result.innerText = "分數:" + s + "-補考";
+    }
+    else {
+        result.innerText = "分數:" + s + "-被當";
+    }
+}
+
+// 迴圈 for
+// 語法
+// for (初始值; 條件-布林值; 迭代器-迴圈結果會執行的區塊) {}
+// 初始值 : 迴圈開始值
+// 條  件 : 迴圈會執行的條件 - 當條件為 ture 執行，false 結束迴圈
+// 迭代器 : 初始值處理 - 遞增++ 
+// var 與 let
+for (let i = 0; i < 10; i++) {
+    console.log("迴圈:"+ i);
+    
+}
+
+// 陣列
+var products = ["冰美式", "卡布奇諾", "摩卡"];
+
+// products.length 陣列.長度(數量) 3
+
+for (let i = 0; i < products.length; i++) {
+    console.log(products[i])   
+}
+
+// 透過類別取得複數元素 - 結果為陣列
+var ps = document.getElementsByClassName("product");
+
+console.log(ps);
+
+for (let i = 0; i < ps.length; i++) {
+    ps[i].innerText = products[i]
+}
+
+// const 常數
+// 不能變更
+const PI = 3.141592653589793;
+// PI = 3.14;      
+// PI = PI + 10錯誤
+
+// let 與 var 差異性
+// let 在同樣的區塊內不能重複 - 安全
+// 只有在區塊內有效
+var countA = 7;
+var countA = 10;
+// let countA = 5
+{
+    let countA = 5;
+    console.log(countA); // 顯示let內
+}
+
+console.log(countA)   // 顯示第二個var
